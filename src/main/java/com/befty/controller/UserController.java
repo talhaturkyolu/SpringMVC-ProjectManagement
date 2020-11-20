52526c31fe6b3a5427bd71c3129751ddbffd9671
Talha Turkyolu <turkyolutalha@gmail.com>
2020-11-19 23:02:43 -0600

    User Create

---
+++ src/main/java/com/befty/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.befty.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/create")
+    public String userCreate(){
+        return "user/create";
+    }
+
+}

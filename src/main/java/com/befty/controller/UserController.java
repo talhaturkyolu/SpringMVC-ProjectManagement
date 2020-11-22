67351be18cfef300a4f683d7a46492a4efe9a45d
Talha Turkyolu <turkyolutalha@gmail.com>
2020-11-22 11:56:49 -0600

    User Create

--- src/main/java/com/befty/controller/UserController.java
+++ src/main/java/com/befty/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.befty.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping("/create")
-    public String userCreate(){
+    @GetMapping({"/create","/add","/initialize"}) // Defined couple of end points
+    public String createUser(){
         return "user/create";
     }
 
